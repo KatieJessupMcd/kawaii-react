cad4068c4bd804c5f96770360dd147af00a83aef
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-05 10:19:59 -0800

    built functionality for random mood on page load

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,24 @@ import { Mug } from 'react-kawaii';
 // import './Home.css';
 
 class Home extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      moods: ["sad", "shocked", "happy", "blissful", "lovestruck", "excited", "ko"]
+    }
+    this.randomMood = this.randomMood.bind(this); 
+  }
+  
+  randomMood(){
+    let randomIdx = Math.floor(Math.random() * this.state.moods.length)
+    let mood = this.state.moods[randomIdx]; 
+    return mood; 
+  }
+  
   render() {
     return (
       <div className="Home">
-        <Mug size={170} mood="happy" color="#A6E191" />
+        <Mug size={170} mood={this.randomMood()} color="#A6E191" />
       </div>
     );
   }
